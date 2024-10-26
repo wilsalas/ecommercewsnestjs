bfc97e6741425f72e55d33ffb6b438cf5107035e
wilsalas <wilsalasgz@gmail.com>
2024-10-26 14:35:41 -0500

    feat: add swagger to main application

---
+++ src/common/enums/environment.enum.ts
@@ -0,0 +1,9 @@
+export enum Environment {
+  ENDPOINT_SECRET_KEY = 'ENDPOINT_SECRET_KEY',
+  TOKEN_SIGN_EXPIRATION_TIME = 'TOKEN_SIGN_EXPIRATION_TIME',
+}
+
+export enum NodeEnv {
+  PRODUCTION = 'production',
+  DEVELOPMENT = 'development',
+}
